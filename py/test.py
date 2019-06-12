df61e73a519c364c8109d7bf539000f0c63461da
Sprinterzzj <zhangzijie.dlut@gmail.com>
2019-06-12 16:18:29 +0900

    upload code

---
+++ py/test.py
@@ -0,0 +1,8 @@
+import tkinter as Tkinter 
+top = Tkinter.Tk() 
+label = Tkinter.Label(top, text='Hello World!') 
+label.pack() 
+Tkinter.mainloop()
+
+from concurrent.futures import Executor
+from concurrent.futures import Future
