f485abae98943834de7582b5d2c56f1f33d12949
Sprinter <zhangzijie.dlut@gmail.com>
2019-09-06 10:33:22 +0900

    upload source code

--- Python-DataStructures/LinkedList.py
+++ Python-DataStructures/LinkedList.py
@@ -6,64 +6,121 @@ class Node(object):
         self.next = next
     
     @classmethod
-    def build(cls, arg_list):
-        if len(arg_list) == 1:
-            head = cls(arg_list[0], None)
+    def build(cls, data_list):
+        """构造单链表. 尾部节点的 next 为 None.
+
+        Parameters
+        ---------
+        data_list: a list of data
+        """
+        if len(data_list) == 0:
+            raise ValueError('`data_list`必须至少有一个元素.')
+        if len(data_list) == 1:
+            head = cls(data_list[0], None)
         else:
-            head = cls(arg_list[0], cls.build(arg_list[1:]))
+            head = cls(data_list[0], cls.build(data_list[1:]))
         return head
     
     def traversal(self):
+        """遍历链表
+        """
         head = self
         while head is not None:
             yield head
             head = head.next
     
-    def searchValue(self, value):
+    def search(self, value):
+        """顺序遍历链表, 返回 Node.data == value
+        的第一个Node, 如果这样的Node不存在就返回NullNode
+        """
         for node in self.traversal():
             if node.data == value:
                 return node
         return NullNode
     
-    def searchNode(self, targetNode):
-        for node in self.traversal():
-            if node is targetNode:
-                return node
-        return NullNode
+    # def searchNode(self, targetNode):
+    #     for node in self.traversal():
+    #         if node is targetNode:
+    #             return node
+    #     return NullNode
     
-    def insertNode(self, before, newNode):
-        newNode.next = before.next
-        before.next = newNode
+    def _insertNode(self, frontNode, newNode):
+        """在`frontNode`节点后面插入新的节点
+        """
+        newNode.next = frontNode.next
+        frontNode.next = newNode
     
-    def deleteNode(self, targetNode):
-        if targetNode is None:
-            return 
-        for node in self.traversal():
-            if node.next is targetNode:
-                node.next = node.next.next
+    def _deleteNode(self, frontNode, targetNode):
+        """删除`targetNode`节点
+        """
+        frontNode.next = targetNode.next
+        del targetNode
     
     def __getitem__(self, index):
         if index <0:
-            raise IndexError('`index` must be >0.')
+            raise IndexError('`index`必须大于零.')
         for i, node in enumerate(self.traversal()):
             if index == i:
                 return node
-        raise IndexError('`index` out of scope.')
+        raise IndexError(f'链表共有 {i+1} 个节点, `index` 为 {index}.')
     
-    def insertIndex(self, index, newNode):
-        if index <=0:
+    def insert(self, index, newNode):
+        """将`newNode`插入到 index 指定的位置.
+        如果 index<0 或 index > 链表总节点数-1, 那么
+        抛出 IndexError.
+        """
+        if index == 0:
             newNode.next = self
-        elif index>0:
+            self = newNode
+        else:
             try:
                 frontNode = self[index-1]
-                newNode.next = frontNode.next
-                frontNode.next = newNode
-            except IndexError:
-                pass
+            except IndexError as e:
+                raise IndexError(e)
+            else:
+                self._insertNode(frontNode, newNode)
+        return self
     
-    def __setitem__(self, index):
-        pass
-    
+    def delete(self, index):
+        """index 指定位置的节点删除.
+        如果 index<0 或 index > 链表总节点数-1, 那么
+        抛出 IndexError.
+        """
+        if index ==0:
+            head = self
+            self = self.next
+            head.next = None
+        else:
+            try:
+                frontNode = self[index-1]
+            except IndexError as e:
+                raise IndexError(e)
+            else:
+                if frontNode.next is not None:
+                    targetNode = frontNode.next
+                    self._deleteNode(frontNode, targetNode)
+        return self
+ 
+    def __setitem__(self, index, newNode):
+        if index == 0: # 将 `newNode` 替换头部元素
+            head = self
+            newNode.next = self.next
+            self = newNode
+            head.next = None
+        else:
+            try:
+                frontNode = self[index-1]
+            except IndexError as e:
+                raise IndexError(e)
+            else:
+                if frontNode.next is None: # 将 `newNode` 插入到尾部
+                    frontNode.next = newNode
+                else: # 替换掉相应位置的节点
+                    targetNode = frontNode.next
+                    frontNode.next = newNode
+                    newNode.next = targetNode.next
+                    del targetNode
+ 
     def __str__(self):
         result = ''
         for i, node in enumerate(self.traversal()):
--- Python-DataStructures/saved_script/LinkedList.py
+++ Python-DataStructures/saved_script/LinkedList.py
@@ -1,47 +1,149 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[49]:
+
+
+get_ipython().run_line_magic('load_ext', 'autoreload')
+get_ipython().run_line_magic('autoreload', '2')
+
+
+# In[59]:
 
 
 from LinkedList import Node
 
 
-# In[2]:
+# In[60]:
 
 
-# Create a single node
+# 构造单个节点
 node = Node(0, None)
 print(node)
 
 
-# In[7]:
+# In[61]:
 
 
-# create a linked list
-head = Node.build(arg_list=list(range(10)))
+# 构造链表
+head = Node.build(data_list=[1,2,3,4,5])
 print(head)
 
 
-# In[8]:
+# In[62]:
 
 
+# 遍历链表
 for nodes in head.traversal():
     print(nodes)
 
 
-# In[13]:
+# In[63]:
 
 
+# getitem
 for i, _ in enumerate(head.traversal()):
     print(head[i])
 
 
-# In[15]:
+# In[64]:
 
 
-newNode = Node(-1, None)
-head.insertNode(0, newNode)
+# setitem
+newNode0 = Node(-1, None)
+newNode3 = Node(-1, None)
+newNode5 = Node(-1, None)
+
+
+# In[65]:
+
+
+head[0] = newNode0
+print(head)
+print(newNode0)
+
+
+# In[66]:
+
+
+# 替换第四个节点
+head = newNode0
+head[3] = newNode3
+print(head)
+
+
+# In[67]:
+
+
+# 插入到末尾
+head[5] = newNode5
+print(head)
+
+
+# In[70]:
+
+
+# 删除头部节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.delete(0)
+print(head)
+print(newHead)
+
+
+# In[71]:
+
+
+# 删除尾部节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.delete(4)
+print(head)
+print(newHead)
+
+
+# In[72]:
+
+
+# 删除中间节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.delete(3)
+print(head)
+print(newHead)
+
+
+# In[73]:
+
+
+# 插入头部节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.insert(0, Node(-1, None))
+print(head)
+print(newHead)
+
+
+# In[74]:
+
+
+# 插入中间节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.insert(4, Node(-1, None))
+print(head)
+print(newHead)
+
+
+# In[75]:
+
+
+# 插入尾部节点
+head = Node.build(data_list=[1,2,3,4,5])
+print(head)
+newHead = head.insert(5, Node(-1, None))
+print(head)
+print(newHead)
 
 
 # In[ ]:
